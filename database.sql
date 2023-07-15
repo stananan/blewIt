cba3d2d5fbaa5c7fff2f8546829c40cc273575ce
Packersfan6 <saahilbakhru@gmail.com>
2023-07-15 14:27:36 -0700

    Created sql file, install.php, and updated register.php
    just created tables for interactions and users.

--- database.sql
+++ database.sql
@@ -1,1 +1,25 @@
+CREATE TABLE IF NOT EXISTS `interactions` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` int,
+    `post_id` int,
+    `interaction_type` text,
+    PRIMARY KEY (`id`)
+);
 
+
+
+CREATE TABLE IF NOT EXISTS `users`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` text,
+    `password` text,
+    `is_admin` BOOLEAN,
+    `create_time` DATETIME,
+    `last_activity_time` DATETIME,
+    PRIMARY KEY (`id`)
+);
+
+-- INSERT INTO `users`
+--     (`username`, `password`, `is_admin`, `create_time`,)
+-- VALUES
+--     ('Packersfan6', '$2y$10$dHxzgTusmEdqFruaHdsroeY0oI7AEH55AMr4ssPr91S4K84Hi3i/e', TRUE, now())
+-- ;
---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <title> Register</title>
+</head>
+
+<body>
+    <form action="registersubmit.php" method = "post">
+        <input type = "text" name = "username">
+        <input type = "text" name = "password">
+    </form>
+</body>
+
+
+</html>
