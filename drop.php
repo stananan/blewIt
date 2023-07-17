8420fd42d4f59c5838230547125622239a9384e9
Packersfan6 <saahilbakhru@gmail.com>
2023-07-17 16:17:13 -0700

    Updated delete sequence
    You have first drop tables with foreign keys.

--- drop.php
+++ drop.php
@@ -10,7 +10,7 @@
 
     try {
         $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-        $dbh->exec('DROP TABLE IF EXISTS bi_users, bi_communities, bi_posts, bi_interactions;');
+        $dbh->exec('DROP TABLE IF EXISTS bi_posts, bi_interactions, bi_users, bi_communities;');
         echo "<p>Successfully dropped databases</p>";
     } catch (PDOException $e) {
         echo "<p>Error: {$e->getMessage()}</p>";
